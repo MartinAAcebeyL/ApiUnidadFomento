e400a9bd69802d1f82d67306f25c3edf13f0721e
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-05-11 13:47:16 -0400

    add api documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from app.routes.uf_route import uf_api
+from flasgger import Swagger
+
 
 def create_app(config):
     app = Flask(__name__)
+    swagger = Swagger()
     app.register_blueprint(uf_api)
     app.config.from_object(config)
     app.app_context().push()
+    swagger.init_app(app)
     return app
--- app/routes/uf_route.py
+++ app/routes/uf_route.py
@@ -1,12 +1,13 @@
 from app.controllers.uf_controllers import get_uf_value
-from . import Blueprint
 from app.utils.responses import response, bad_request, not_found
-
+from flask import Blueprint
+from flasgger import swag_from
 
 uf_api = Blueprint('uf_api', __name__, url_prefix='/api/v1/uf')
 
 
 @uf_api.route('/<string:date>', methods=['GET'])
+@swag_from('./documentation/uf/get_uf.yaml')
 def get_uf(date: str):
     try:
         return response(get_uf_value(date), "UF value")
