dd2d17ed523a77ac28b38eafb2a6b712a01de3ac
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2023-07-17 21:11:15 -0400

    add guvicorn

--- dockerfile
+++ dockerfile
@@ -2,4 +2,5 @@ FROM python:3.9
 WORKDIR /app
 COPY . /app
 RUN pip install -r requirements.txt
-CMD ["python", "manage.py"]
+EXPOSE 8000
+CMD ["gunicorn", "manage:app", "-b", "0.0.0.0:8000"]
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app
 from config import config
 
+app = create_app(config['deploy'])
 if __name__ == '__main__':
-    app = create_app(config['deploy'])
     app.run(host='0.0.0.0')
 
